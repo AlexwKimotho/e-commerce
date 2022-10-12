09299aa4a70100291d2d0c01ddcf1cec5e3afd9e
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-10-12 10:35:32 +0300

    Completed the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">
               BRAND NEW OUTFITS
-            {/* </h5>
+            </h5>
             <p className="card-text lead fs-3" >
               Beauty is composed of an eternal, invariable element whose
               quantity is extremely difficult to determine, and a relative
@@ -31,4 +31,4 @@ const Home = () => {
     </div>
   );
 };
-export default Home; */}
+export default Home;
