48c0fd025303aad971e5174e6c8ef662175ab11e
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-10-12 15:28:17 +0300

    Project Completed

---
+++ docs/precache-manifest.fca8162e661038f75440f212c5b3cc00.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "18ab92e2a679136999d0",
+    "url": "/static/css/main.4b3b5a03.chunk.css"
+  },
+  {
+    "revision": "18ab92e2a679136999d0",
+    "url": "/static/js/main.18ab92e2.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "cad4465cc7dab52d3288",
+    "url": "/static/css/2.3f9f2ff0.chunk.css"
+  },
+  {
+    "revision": "cad4465cc7dab52d3288",
+    "url": "/static/js/2.cad4465c.chunk.js"
+  },
+  {
+    "revision": "3220869980800ef67035",
+    "url": "/static/js/runtime~main.32208699.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ff8ea2b05011de885c164d4cce6c53e1",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import React from 'react';
 function App() {
   return (
     <>
+
     <Navbar/>
     <Home/>
     </>
