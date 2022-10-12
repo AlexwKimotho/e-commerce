6631681f2c81e5bf6c79d737c330a7636d2d7fe6
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-10-12 10:25:43 +0300

    styled the navigation bar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,12 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className=" navbar-toggler-icon "></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id=" navbarSupportedContent ">
+            <ul className=" navbar-nav mx-auto mb-2 mb-lg-0 ">
+              <li className=" nav-item ">
+                <a className=" nav-link active " aria-current=" page " href="#">
                   Home
                 </a>
               </li>
