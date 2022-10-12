1d99f02df1aa19040d9df037e0b6627aa2854bbe
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-10-12 10:34:07 +0300

    added a picure to my page and soe text to impove visibility on the landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,34 @@
-// import React from "react";
-// import Products from "./Products";
+import React from "react";
+import Products from "./Products";
 
-// const Home = () => {
-//   return (
-//     <div className="hero">
-//       <div className="card text-bg-dark text-white border-0">
-//         <img
-//           src="/assets/bg.jpg"
-//           className="card-img"
-//           alt="background"
-//           height="600"
-//         />
-//         <div className="card-img-overlay d-flex flex-column justify-content-center">
-//           <div className="container">
-//             <h5 className="card-title display-3 fw-bolder mb-0">
-//               BRAND NEW OUTFITS
-//             </h5>
-//             <p className="card-text lead fs-3" >
-//               Beauty is composed of an eternal, invariable element whose
-//               quantity is extremely difficult to determine, and a relative
-//               element which might be, either by turns or all at once, period,
-//               fashion, moral, passion.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card text-bg-dark text-white border-0">
+        <img
+          src="/assets/bg.jpg"
+          className="card-img"
+          alt="background"
+          height="600"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              BRAND NEW OUTFITS
+            {/* </h5>
+            <p className="card-text lead fs-3" >
+              Beauty is composed of an eternal, invariable element whose
+              quantity is extremely difficult to determine, and a relative
+              element which might be, either by turns or all at once, period,
+              fashion, moral, passion.
+            </p>
+          </div>
+        </div>
+      </div>
 
-//       <Products/>
+      <Products/>
 
-//     </div>
-//   );
-// };
-// export default Home;
+    </div>
+  );
+};
+export default Home; */}
